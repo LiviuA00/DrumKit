28586913105f6a7c324e6b0e5305d635a614aa79
LiviuA00 <liviuanton418@yahoo.com>
2023-11-13 13:07:42 +0200

    pressing buttons and animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 var numOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numOfDrumButtons; i++) {
@@ -5,10 +6,21 @@ for (var i = 0; i < numOfDrumButtons; i++) {
 }
 
 function handleClick() {
-
   var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+}
 
-  switch (buttonInnerHTML) {
+
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+
+
+function makeSound(key) {
+  switch (key) {
     case "w":
       var tom1 = new Audio("./sounds/tom-1.mp3");
       tom1.play();
@@ -47,4 +59,12 @@ function handleClick() {
     default: console.log(buttonInnerHTML);
       break;
   }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function() { activeButton.classList.remove("pressed");}, 100);
+  
 }
